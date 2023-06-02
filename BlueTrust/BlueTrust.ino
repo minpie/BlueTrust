d7bc8e12260db17ca8a4974a28ac20f61fa2c3d4
minpie <xhxhdwkd@gmail.com>
2023-06-02 10:46:11 +0900

    edited pin number, added motor control, etc

--- BlueTrust/BlueTrust.ino
+++ BlueTrust/BlueTrust.ino
@@ -10,18 +10,19 @@
 
 
 // Pin:
-#define PINI2CSDA 0 // I2C: SDA pin
-#define PINI2CSCL 0 // I2C: SCL pin
-#define PINSPIMOSI 0 // SPI: MOSI pin
-#define PINSPIMISO 0 // SPI: MISO pin
-#define PINSPISCLK 0 // SPI: SCLK pin
-#define PINTDSANALOG 0 // TDS: Analog pin
-#define PINBTRX 0 // Bluetooth: RX pin
-#define PINBTTX 0 // Bluetooth: TX pin
-#define PINJOYVER 0 // Joystick: vertical pin
-#define PINJOYHOR 0 // Joystick: horizontal pin
-#define PINJOYBTN 0 // Joystick: button pin
-#define PINLEDSTRIP 8 // LED Strip: control pin
+#define PINI2CSDA 24 // I2C: SDA pin
+#define PINI2CSCL 25 // I2C: SCL pin
+#define PINSPIMOSI 19 // SPI: MOSI pin
+#define PINSPIMISO 20 // SPI: MISO pin
+#define PINSPISCLK 18 // SPI: SCLK pin
+#define PINTDSANALOG 27 // TDS: Analog pin
+#define PINBTRX 10 // Bluetooth: Use to RX pin
+#define PINBTTX 9 // Bluetooth: Use to TX pin
+#define PINJOYVER 28 // Joystick: vertical pin
+#define PINJOYHOR 29 // Joystick: horizontal pin
+#define PINJOYBTN 11 // Joystick: button pin
+#define PINLEDSTRIP 6 // LED Strip: control pin
+#define PINFANEN 12 // Motor Driver(Fan): EN pin
 
 
 // Define:
@@ -31,6 +32,9 @@
 
 // Function:
 void SetLEDStrip(uint8_t ledNumber, uint8_t r, uint8_t g, uint8_t b); // Set (ledNumber)LED's color
+void SetFanStop(void); // Set Fan to stop
+void SetFanRunFull(void); // Set Fan to run at full speed
+void SetFanWithSpeed(uint8_t spd); // Set Fan with target speed
 
 
 // Object:
@@ -48,10 +52,16 @@ void setup(void) {
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
 
+  // pin mode:
+  pinMode(PINFANEN, OUTPUT); // motor driver en pin
+
   // LED Strip:
   ledStrip.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
   ledStrip.setBrightness(LEDBRIGHTNESS);
   ledStrip.clear(); // Set all pixel colors to 'off'
+
+  // TEST:
+  SetFanRunFull();
 }
 void loop(void) {
   // TEST:
@@ -71,4 +81,17 @@ void SetLEDStrip(uint8_t ledNumber, uint8_t r, uint8_t g, uint8_t b){
   // Set (ledNumber)LED's color
   ledStrip.setPixelColor(ledNumber, ledStrip.Color(r, g, b));
   ledStrip.show();   // Send the updated pixel colors to the hardware.
+}
+
+void SetFanStop(void){
+  // Set Fan to stop
+  analogWrite(PINFANEN, 0);
+}
+void SetFanRunFull(void){
+  // Set Fan to run at full speed
+  analogWrite(PINFANEN, 255);
+}
+void SetFanWithSpeed(uint8_t spd){
+  // Set Fan with target speed
+  analogWrite(PINFANEN, spd);
 }
