f3da687f7b445e83f0a685b749a556044a7e24ba
minpie <xhxhdwkd@gmail.com>
2023-06-23 12:23:32 +0900

    Added LCD, TDS, Temperature Sensor Code

--- BlueTrust/BlueTrust.ino
+++ BlueTrust/BlueTrust.ino
@@ -9,6 +9,9 @@
 #include <Adafruit_NeoPixel.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <stdint.h>
+#include <LCD.h>
+#include <SPI.h>
 
 
 // Pin:
@@ -17,7 +20,7 @@
 #define PINSPIMOSI 0 // SPI: MOSI pin
 #define PINSPIMISO 0 // SPI: MISO pin
 #define PINSPISCLK 0 // SPI: SCLK pin
-#define PINTDSANALOG 0 // TDS: Analog pin
+#define PINTDSANALOG A0 // TDS: Analog pin
 #define PINBTRX 0 // Bluetooth: Use to RX pin
 #define PINBTTX 0 // Bluetooth: Use to TX pin
 #define PINJOYVER 0 // Joystick: vertical pin
@@ -31,6 +34,18 @@
 // Define:
 #define AMOUNTLEDSTRIP 60 // Amount of LED in LED Strip
 #define LEDBRIGHTNESS 240 // LED Strip's brightness, max: 255 min: 0
+#define TEMPSENMAX 30 // Tempereture Sensor Max
+#define TEMPSENMIN 20 // Tempereture Sensor Min
+
+
+
+
+#define VREF 5.0      // analog reference voltage(Volt) of the ADC
+#define SCOUNT  3           // sum of sample point
+int analogBuffer[SCOUNT];    // store the analog value in the array, read from ADC
+int analogBufferTemp[SCOUNT];
+int analogBufferIndex = 0,copyIndex = 0;
+float averageVoltage = 0,tdsValue = 0,temperature = 25;
 
 
 // Function:
@@ -38,6 +53,7 @@ void SetLEDStrip(uint8_t ledNumber, uint8_t r, uint8_t g, uint8_t b); // Set (le
 void SetFanStop(void); // Set Fan to stop
 void SetFanRunFull(void); // Set Fan to run at full speed
 void SetFanWithSpeed(uint8_t spd); // Set Fan with target speed
+int getMedianNum(int bArray[], int iFilterLen);
 
 
 // Object:
@@ -51,7 +67,7 @@ DallasTemperature tempSensors(&tempOneWire);
 uint8_t rColor = 0;
 
 
-/* ### main ### */
+// ### main ###
 void setup(void) {
   // TEST:
   pinMode(LED_BUILTIN, OUTPUT);
@@ -67,6 +83,20 @@ void setup(void) {
 
   // TEST:
   //SetFanRunFull();
+  SPI.setDataMode(SPI_MODE3);
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setClockDivider(SPI_CLOCK_DIV4);
+  SPI.begin();
+
+    
+  Tft.lcd_init();                                      // init TFT library
+  Tft.setRotation(Rotation_90_D);
+  //Tft.lcd_display_string(60, 120, (const uint8_t *)"Hello, world !", FONT_1608, RED);
+  //Tft.lcd_display_string(30, 152, (const uint8_t *)"2.8' TFT Touch Shield", FONT_1608, RED);
+
+
+  //Serial.begin(115200);
+  pinMode(PINTDSANALOG,INPUT);
 }
 void loop(void) {
   // TEST:
@@ -77,11 +107,39 @@ void loop(void) {
   }
   tempSensors.requestTemperatures();
   float temp = tempSensors.getTempCByIndex(0);
-  uint8_t panSpd = map(constrain(temp, 27, 30), 27, 30, 0, 255);
+  temperature = temp;
+  uint8_t panSpd = map(constrain(temp, TEMPSENMIN, TEMPSENMAX), TEMPSENMIN, TEMPSENMAX, 120, 255);
   SetFanWithSpeed(panSpd);
+  
+
+  static unsigned long analogSampleTimepoint = millis();
+  if(millis()-analogSampleTimepoint > 40U){
+    //every 40 milliseconds,read the analog value from the ADC
+    analogSampleTimepoint = millis();
+    analogBuffer[analogBufferIndex] = analogRead(PINTDSANALOG);    //read the analog value and store into the buffer
+    analogBufferIndex++;
+    if(analogBufferIndex == SCOUNT) 
+      analogBufferIndex = 0;
+  }   
+  static unsigned long printTimepoint = millis();
+  if(millis()-printTimepoint > 800U){
+    printTimepoint = millis();
+    for(copyIndex=0;copyIndex<SCOUNT;copyIndex++)
+      analogBufferTemp[copyIndex]= analogBuffer[copyIndex];
+    averageVoltage = getMedianNum(analogBufferTemp,SCOUNT) * (float)VREF / 1024.0; // read the analog value more stable by the median filtering algorithm, and convert to voltage value
+    float compensationCoefficient=1.0+0.02*(temperature-25.0);    //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0));
+    float compensationVolatge=averageVoltage/compensationCoefficient;  //temperature compensation
+    tdsValue=(133.42*compensationVolatge*compensationVolatge*compensationVolatge - 255.86*compensationVolatge*compensationVolatge + 857.39*compensationVolatge)*0.5; //convert voltage value to tds value
+
+
+    Tft.lcd_clear_screen(WHITE);
+    Tft.lcd_display_string(60, 120, (const uint8_t *)"TDS Value(ppm):", FONT_1608, RED);
+    Tft.lcd_display_num(30, 152, (const uint32_t)tdsValue, 5, FONT_1608, RED);
+  }
+
   delay(200);
 }
-/* ### main ### */
+// ### main ###
 
 
 // Function:
@@ -102,4 +160,26 @@ void SetFanRunFull(void){
 void SetFanWithSpeed(uint8_t spd){
   // Set Fan with target speed
   analogWrite(PINFANEN, spd);
+}
+
+
+int getMedianNum(int bArray[], int iFilterLen){
+  int bTab[iFilterLen];
+  for (byte i = 0; i<iFilterLen; i++)
+    bTab[i] = bArray[i];
+  int i, j, bTemp;
+  for (j = 0; j < iFilterLen - 1; j++) {
+    for (i = 0; i < iFilterLen - j - 1; i++) {
+      if (bTab[i] > bTab[i + 1]) {
+        bTemp = bTab[i];
+        bTab[i] = bTab[i + 1];
+        bTab[i + 1] = bTemp;
+      }
+    }
+  }
+  if ((iFilterLen & 1) > 0)
+    bTemp = bTab[(iFilterLen - 1) / 2];
+  else
+    bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
+  return bTemp;
 }
